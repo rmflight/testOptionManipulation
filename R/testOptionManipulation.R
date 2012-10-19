7ec76c1268ad915e1d17463396d1348d92764479
Robert M Flight <r.flight79@gmail.com>
2012-10-19 10:46:01 -0400

    put in the option to change the strings

--- R/testOptionManipulation.R
+++ R/testOptionManipulation.R
@@ -1,7 +1,16 @@
 cleanOption <- function(){
-	empty <- list()
-	class(empty) <- "bibentry"
+	empty <- character(0)
 	options(testOption = empty)
 }
 
 
+addOption <- function(inputString){
+	if (is.null(getOption("testOption"))){
+		cleanOption
+	}
+	
+	oldOption <- getOption("testOption")
+	newOption <- c(oldOption, inputString)
+	options(testOption = newOption)
+	
+}
